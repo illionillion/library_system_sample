4ae1f647942b3fdf2fe1c4289a9de7444135c28b
yuto ichinose <yuto.0526.sano@gmail.com>
2023-06-30 22:14:26 +0900

    first commit

---
+++ www/html/db/index.php
@@ -0,0 +1,29 @@
+<?php
+if(!isset($_SESSION)){ session_start(); }
+if (getenv('DOCKER_ENV')) {
+    // Docker Compose環境の場合
+    $host = 'db'; // Docker Composeのサービス名を指定
+} else {
+    // ローカル環境（XAMPPなど）の場合
+    $host = 'localhost'; // ローカルホストを指定
+}
+$dbname = 'php2023_234201'; // データベース名
+$username = 'user_234201'; // ユーザー名
+$password = '234201'; // パスワード
+
+try {
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+
+    // PDOオブジェクトを作成し、データベースに接続する
+    $pdo = new PDO($dsn, $username, $password, $options);
+
+    // 接続成功時の処理
+    echo "データベースに接続しました。";
+} catch (PDOException $e) {
+    // 接続失敗時の処理
+    die("データベース接続エラー: " . $e->getMessage());
+}
---
+++ www/html/login/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ログイン</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/login.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h2>ログイン</h2>
+                <form action="/auth/index.php" method="POST">
+                    <div class="form-group">
+                        <label class="w-100" for="username">ユーザー名</label>
+                        <input type="text" class="form-control" id="username" name="username" required>
+                    </div>
+                    <div class="form-group">
+                        <label class="w-100" for="password">パスワード</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <div class="py-3"><button type="submit" class="btn btn-primary">ログイン</button></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

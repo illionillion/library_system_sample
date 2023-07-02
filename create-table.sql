a3a48d91af8af6ebe71133179c4dafddb0a0dbdf
yuto ichinose <yuto.0526.sano@gmail.com>
2023-07-02 10:33:39 +0900

    テーブル修正

--- create-table.sql
+++ create-table.sql
@@ -3,13 +3,13 @@ ALTER USER 'user_234201'@'%' IDENTIFIED WITH mysql_native_password BY '234201';
 
 -- users
 CREATE TABLE users (
-    user_id INT PRIMARY KEY,
-    user_name VARCHAR(255),
-    password LONGTEXT,
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(255) NOT NULL,
+    password LONGTEXT NOT NULL,
     phone_number VARCHAR(20),
     email VARCHAR(255),
     role INT
 );
 -- 初期データ（初回導入時はこれを実行してシステムにログインできるようにする）
-INSERT INTO users (user_id, user_name, password, phone_number, email, role)
-VALUES (1, 'Admin', SHA2('password', 256), '1234567890', 'admin@example.com', 0);
+INSERT INTO users (user_name, password, phone_number, email, role)
+VALUES ('Admin', SHA2('password', 256), '1234567890', 'admin@example.com', 0);
